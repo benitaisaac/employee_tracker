de60acd53419f60d275a3cde46280edfc2110744
benitaisaac <benita.isaac@gmail.com>
2024-01-28 17:35:29 -0800

    added data into seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO department (name)
+VALUES
+    ("Human Resources"),
+    ("Sales"),
+    ("Engineering"),
+    ("Marketing"),
+    ("IT"),
+    ("Sales");
+
+INSERT INTO role (title, salary)
+	VALUES
+    ('HR Manager', 60000),
+    ('Financial Analyst', 70000),
+    ('Marketing Specialist', 65000),
+    ('Software Engineer', 110000),
+    ('Sales Representative', 80000);
+
+
+INSERT INTO employee (first_name, last_name)
+	VALUES
+    ('John', 'Doe'),
+    ('Jane', 'Smith'),
+    ('Mike', 'Johnson'),
+    ('Emily', 'Davis'),
+    ('Alex', 'Brown'),
+    ('Emma', 'Williams'),
+    ('Ryan', 'Miller'),
+    ('Olivia', 'Jones'),
+    ('Ethan', 'Moore'),
+    ('Sophia', 'Wilson'),
+    ('Logan', 'Taylor'),
+    ('Ava', 'Anderson');
